600ef0b867851b80bb63fccc9867a798ec3ba956
FuentesCelina <fuentescelina01@gmail.com>
2024-05-29 13:19:43 -0300

    se pueden buscar emprendimientos

--- jss/buscar.js
+++ jss/buscar.js
@@ -1,33 +1,31 @@
-
-const url = 'json/emprendimientos.json';
-const url_colaboradores = 'json/colaboradores.json';
-var ultimoId = 0;
-/*
-document.addEventListener('DOMContentLoaded', function () {
-    filtrarJSONEmp("");//se muestran todos
-    var buscar = document.getElementById("searchField");
-    searchField.addEventListener('input', function (event) {
-        var emp_abuscar = event.target.value;
-        filtrarJSONEmp(emp_abuscar.toLowerCase());
-    });
-});*/
-
+const emprendimientos = [];
 fetch('https://raw.githubusercontent.com/FuentesCelina/IS-Portal-Emprendimientos/main/json/emprendimientos.json')
     .then(response => response.json())
     .then(data => {
         // Llamar a la función para mostrar los productos
         crearNuevoEmp(data);
     })
-    .catch(error => console.error('Error al cargar los productos:', error));
+    .catch(error => console.error('Error al cargar los emprendimientos:', error));
+var ultimoId = 0;
+
+document.addEventListener('DOMContentLoaded', function () {
+    filtrarJSONEmp("");//se muestran todos
+    var buscar = document.getElementById("searchField");
+    buscar.addEventListener('input', function (event) {
+        var emp_abuscar = event.target.value;
+        filtrarJSONEmp(emp_abuscar.toLowerCase());
+    });
+});
+
+
 function filtrarJSONEmp(filtrado) {
-    fetch(url)
-        .then(function (res) {
-            return res.json();
-        })
-        .then(function (data) {
-            let emprendimientos = [];
+    fetch('https://raw.githubusercontent.com/FuentesCelina/IS-Portal-Emprendimientos/main/json/emprendimientos.json')
+        .then(response => response.json())
+        .then(data => {
+            // Llamar a la función para mostrar los productos
+            const emprendimientos=[];
             data.forEach((item) => {
-                if (item.rubro.toLowerCase() === filtrado || filtrado==="") {
+                if (item.rubro.toLowerCase() === filtrado || filtrado === "") {
                     let obj = {
                         nombre: item.nombre,
                         descripcion: item.descripcion,
@@ -43,4 +41,5 @@ function filtrarJSONEmp(filtrado) {
             })
             crearNuevoEmp(emprendimientos);
         })
+        .catch(error => console.error('Error al cargar los emprendimientos:', error));
 }
